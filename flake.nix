9ab9884ecbbb4fa787f4eae4a808161cff0d27a5
Lucy Satheesan <me@lucys.dev>
2023-03-11 15:58:06 -0500

    fix(flake.nix): pass inputs to config

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
   outputs = { self, nixpkgs, ... }@attrs : {
     nixosConfigurations.carmine = nixpkgs.lib.nixosSystem {
       system = "aarch64-linux";
+      specialArgs = attrs; # pass the inputs to the configuration
       modules = [ ./hosts/carmine/configuration.nix ];
     };
   };
