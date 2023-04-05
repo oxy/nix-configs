63f01115cee5a64410b2b81ccfe74c39017e1241
Lucy Satheesan <me@lucys.dev>
2023-04-05 00:44:43 -0400

    jellyfin: hardware encoding

--- crumbs/jellyfin.nix
+++ crumbs/jellyfin.nix
@@ -11,4 +11,18 @@
   systemd.services.jellyfin.path = with pkgs; [
     jellyfin-ffmpeg
   ];
+
+  # hardware decoding requires opengl to be enabled
+  # (because /run/opengl-drivers/ needs it)
+  hardware.opengl.enable = lib.mkDefault true;
+
+  # HACK: this uses the intel-ucode flag, which is janky
+  # but all my systems with intel-ucode have intel GPUs so :)
+  hardware.opengl.extraPackages = with pkgs; lib.mkIf config.hardware.cpu.intel.updateMicrocode [
+    intel-media-driver
+    intel-media-sdk
+    intel-compute-runtime
+    # TODO: package oneapi-vpl
+    intel-ocl
+  ];
 }
