220b1ad4cd48a5aa1f3d7c1dfc608202b54163d3
Lucy Satheesan <me@lucys.dev>
2023-04-12 19:16:21 -0400

    crumbs: move all into base

--- crumbs/base.nix
+++ crumbs/base.nix
@@ -1,13 +1,13 @@
 { config, lib, pkgs, impermanence, ... }:
 
-{ 
+{
+  imports = [
+    impermanence.nixosModule
+  ];
+
   # all of the required services
   config = lib.mkMerge [
-    # unconditional things
-    import impermanence.nixosModule {}
-    
     {
-
       time.timeZone = "America/New_York";
 
       nix.settings.allowed-users = [ "@wheel" ];
@@ -58,33 +58,169 @@
       networking.nftables.enable = true;
     }
 
-    # conditioned on other config options
+    # if xserver is enabled, pull in gnome and set defaults
     (lib.mkIf config.services.xserver.enable {
-      imports = [ ./desktop.nix ];
+      # Enable the GNOME Desktop Environment.
+      services.xserver.displayManager.gdm.enable = true;
+      services.xserver.desktopManager.gnome.enable = true;
+
+      # Configure keymap in X11
+      services.xserver = {
+        layout = "us";
+        xkbVariant = "";
+      };
+
+      # Enable sound with pipewire.
+      sound.enable = true;
+      hardware.pulseaudio.enable = false;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = false;
+
+        wireplumber.enable = true;
+      };
     })
 
+    # if jellyfin is enabled, set up jellyfin + ffmpeg
     (lib.mkIf config.services.jellyfin.enable {
-      imports = [ ./jellyfin.nix ];
+      environment.persistence."/nix/persist".directories = [
+        "/var/lib/jellyfin"
+        "/var/cache/jellyfin"
+      ];
+
+      systemd.services.jellyfin.path = with pkgs; [
+        jellyfin-ffmpeg
+      ];
+
+      # hardware decoding requires opengl to be enabled
+      # (because /run/opengl-drivers/ needs it)
+      hardware.opengl.enable = lib.mkDefault true;
+
+      # HACK: this uses the intel-ucode flag, which is janky
+      # but all my systems with intel-ucode have intel GPUs so :)
+      hardware.opengl.extraPackages = with pkgs; lib.mkIf config.hardware.cpu.intel.updateMicrocode [
+        intel-media-driver
+        intel-media-sdk
+        intel-compute-runtime
+        # TODO: package oneapi-vpl
+        intel-ocl
+      ];
     })
 
+    # if openssh is enabled, set up default sshd config
     (lib.mkIf config.services.openssh.enable {
-      imports = [ ./sshd.nix ];
+      services.openssh = {
+        allowSFTP = true;
+        extraConfig = ''
+          AllowTcpForwarding yes
+          AllowAgentForwarding no
+          AllowStreamLocalForwarding no
+          AuthenticationMethods publickey
+        '';
+      };
+
+      services.openssh.settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        X11Forwarding = false;
+      };
+
+      environment.persistence."/nix/persist".files =
+        lib.concatMap (key: [ key.path (key.path + ".pub") ]) config.services.openssh.hostKeys;
+
+      networking.firewall.allowedTCPPorts = [ 22 ];
+      networking.firewall.allowedUDPPorts = [  ];
     })
 
+    # if samba is enabled, set up discovery and persist db
     (lib.mkIf config.services.samba.enable {
-      imports = [ ./samba.nix ];
+      services.samba.openFirewall = true;
+      environment.persistence."/nix/persist".directories = [ "/var/lib/samba" ];
+
+      # enable wsdd
+      services.samba-wsdd.enable = true;
+      networking.firewall.allowedTCPPorts = [ 5357 ];
+      networking.firewall.allowedUDPPorts = [ 3702 ];
     })
 
+    # if tailscale is enabled, set up persistence and firewall bypass
     (lib.mkIf config.services.tailscale.enable {
-      imports = [ ./tailscale.nix ];
+      environment.systemPackages = [ pkgs.tailscale ];
+      environment.persistence."/nix/persist".directories = [ "/var/lib/tailscale" ];
+      networking.firewall.trustedInterfaces = [ "tailscale0" ];
     })
 
+    # if transmission is enabled, set up mullvad and transmission
     (lib.mkIf config.services.transmission.enable {
-      imports = [ ./mullvad.nix ./transmission.nix ];
+      # connect to mullvad
+      systemd.network.netdevs."10-wg0" = {
+        netdevConfig = {
+          Kind = "wireguard";
+          Name = "wg0";
+        };
+        wireguardConfig = {
+          ListenPort = 51820;
+          PrivateKeyFile = "/nix/secrets/mullvad.key";
+          RouteTable = "main";
+          RouteMetric = 8192;
+        };
+        wireguardPeers = [{
+          wireguardPeerConfig = {
+            PublicKey = "6CwMg2aoKNSFFcIsW3R3SY5T6fBYwoRFifl8ZVlw+Vg=";
+            AllowedIPs = [ "0.0.0.0/0" "::0/0" ];
+            Endpoint = "66.63.167.194:51820";
+          };
+        }];
+      };
+
+      # set up default gateway / route
+      systemd.network.networks.wg0 = {
+        matchConfig.Name = "wg0";
+        address = lib.mkDefault [
+          "10.65.153.40/32"
+          "fc00:bbbb:bbbb:bb01::2:9927/128"
+        ];
+        DHCP = "no";
+        dns = [ "100.64.0.7" ];
+        gateway = [ "10.64.0.1" ];
+        networkConfig = {
+          IPv6AcceptRA = false;
+        };
+      };
+
+      # TODO: set up transmission
     })
 
+    # set up networkd if we aren't using networkmanager
     (lib.mkIf (!config.networking.networkmanager.enable) {
-      imports = [ ./networkd.nix ];
+      # networking
+      networking = {
+        useDHCP = false;
+        useNetworkd = true;
+      };
+
+      systemd.network.networks = let
+      networkConfig = {
+        DHCP = "yes";
+      }; in {
+        "40-wired" = {
+          enable = true;
+          name = "en*";
+          inherit networkConfig;
+          dhcpV4Config.RouteMetric = 1024;
+        };
+      } // lib.optionalAttrs (config.networking.wireless.enable) {
+        "40-wireless" = {
+          enable = true;
+          name = "wl*";
+          inherit networkConfig;
+          dhcpV4Config.RouteMetric = 2048;
+        };
+      };
     })
   ];
 
--- crumbs/transmission.nix
+++
@@ -1,5 +0,0 @@
-{
-  services.transmission.settings = {
-    rpc-whitelist = "";
-  };
-}
