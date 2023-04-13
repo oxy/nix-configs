dbf723fb91081d27a7dabdcd98b50c4221d3049c
Lucy Satheesan <me@lucys.dev>
2023-04-13 19:50:03 -0400

    base: switch to mullvad vpn service

--- crumbs/base.nix
+++ crumbs/base.nix
@@ -175,68 +175,12 @@
 
     # if transmission is enabled, set up mullvad and transmission
     (lib.mkIf config.services.transmission.enable {
-      # connect to mullvad
-      systemd.network.netdevs."10-mullvad" = {
-        netdevConfig = {
-          Kind = "wireguard";
-          Name = "wgmullvad";
-        };
-        wireguardConfig = {
-          ListenPort = 51820;
-          PrivateKeyFile = "/nix/secrets/mullvad.key";
-          RouteTable = "main";
-          RouteMetric = 8192;
-        };
-        wireguardPeers = [{
-          wireguardPeerConfig = {
-            PublicKey = "6CwMg2aoKNSFFcIsW3R3SY5T6fBYwoRFifl8ZVlw+Vg=";
-            AllowedIPs = [ "0.0.0.0/0" "::0/0" ];
-            Endpoint = "66.63.167.194:51820";
-          };
-        }];
-      };
-
-      # set up default gateway / route
-      systemd.network.networks.wgmullvad = {
-        matchConfig.Name = "wgmullvad";
-        address = lib.mkDefault [
-          "10.65.153.40/32"
-          "fc00:bbbb:bbbb:bb01::2:9927/128"
-        ];
-        DHCP = "no";
-        dns = [ "100.64.0.7" ];
-        gateway = [ "10.64.0.1" ];
-        networkConfig = {
-          IPv6AcceptRA = false;
+      services.mullvad-vpn.enable = true;
+      systemd.services."mullvad-daemon" = {
+        unitConfig = {
+          JoinsNamespaceOf = "netns@transmission.service";
         };
       };
-
-      systemd.services."attach-vpn@" = {
-        description = "Attach to Mullvad VPN";
-        requires = [ "netns@%i.service" ];
-        after = [ "netns@%i.service" ];
-
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-
-          ExecStart = [
-            # associate NIC with netns
-            "${pkgs.iproute}/bin/ip link set wgmullvad netns %i"
-
-            # bring up NIC
-            "${pkgs.iproute}/bin/ip netns exec %i ${pkgs.iproute}/bin/ip link set up dev wgmullvad"
-            
-            # add address / gateway
-            "${pkgs.iproute}/bin/ip netns exec %i ${pkgs.iproute}/bin/ip addr add 10.65.153.40 broadcast 10.64.0.255 dev wgmullvad"
-            "${pkgs.iproute}/bin/ip netns exec %i ${pkgs.iproute}/bin/ip route add default via 10.64.0.1 dev wgmullvad"
-          ];
-
-          ExecStop = "${pkgs.iproute}/bin/ip netns exec %i ${pkgs.iproute}/bin/ip link set down dev wgmullvad";
-        };
-      };
-
-      # TODO: set up transmission
     })
 
     # set up networkd if we aren't using networkmanager
